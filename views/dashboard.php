7331740746dba7cdcf7bcd1b1b34e85603afde24
Lavender <gercermagden@gmail.com>
2025-04-16 00:18:58 -0500

    Add dashboard.php file with initial layout and includes

---
+++ views/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+?>
+
+<?php include __DIR__ . '/../includes/header.php'; ?>
+
+<?php include __DIR__ . '/../includes/sidebar.php'; ?>
+<div class="flex-grow-1 app-content">
+    <div id="main-content" class="p-4" style="margin-left: 300px;">
+        <!-- Aquí se cargará el contenido de cada página -->
+    </div>
+</div>
+
+<?php include __DIR__ . '/../includes/scripts.php'; ?>
