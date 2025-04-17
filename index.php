21f03761ac6697cbb43d2a977525a86b3d990611
Lavender <gercermagden@gmail.com>
2025-04-17 17:01:03 -0500

    Initialize index.php with session start and include necessary files

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+?>
+
+
+<?php require_once __DIR__ . '/includes/header.php' ?>
+<?php require_once __DIR__ . '/includes/navbar.php' ?>
+
+<?php require_once __DIR__ . '/includes/scripts.php' ?>
