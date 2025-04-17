c2eb6d65a2101bc04e4e3b91d06e2ca0d6c9aa99
Lavender <gercermagden@gmail.com>
2025-04-17 17:01:45 -0500

    Update apiRequest import version in authService.js for consistency

--- js/api/authService.js
+++ js/api/authService.js
@@ -1,5 +1,9 @@
-import { apiRequest } from './apiRequest.js?v=1';
+import { apiRequest } from './apiRequest.js?v=2';
 
 export function logearUsuario(data) {
     return apiRequest('auth', 'login', 'POST', data);
+}
+
+export function deslogearUsuario() {
+    return apiRequest('auth', 'logout', 'POST', null);
 }
