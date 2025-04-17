19089a5867841d9ccca0f15666095658c2d00d67
Lavender <gercermagden@gmail.com>
2025-04-16 22:33:47 -0500

    Add versioning to apiRequest import in usuarioService.js for cache busting

--- js/api/usuarioService.js
+++ js/api/usuarioService.js
@@ -1,4 +1,4 @@
-import { apiRequest } from './apiRequest.js';
+import { apiRequest } from './apiRequest.js?v=1';
 
 export function registrarUsuario(data) {
     // Por ejemplo: data = { nombre, telefono, email, fecha_logeo, contrasena, id_rol }
