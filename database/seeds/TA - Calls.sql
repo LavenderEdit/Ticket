bca75beb607800ced633125f8e0be4945e5be1cb
Lavender <gercermagden@gmail.com>
2025-04-16 22:33:05 -0500

    Fix casing of stored procedure calls in TA - Calls.sql

--- database/seeds/TA - Calls.sql
+++ database/seeds/TA - Calls.sql
@@ -1,5 +1,12 @@
-CALL sp_visualizar_Roles();
-CALL sp_visualizar_Permisos();
-CALL sp_visualizar_Rol_Permiso();
+CALL sp_visualizar_roles();
+CALL sp_visualizar_permisos();
+CALL sp_visualizar_rol_Permiso();
+
+CALL sp_autenticar_usuario("juan.perez@example.com");
+
+CALL sp_visualizar_usuario();
+CALL sp_eliminar_usuario(4);
+
+CALL sp_crear_usuario('Joan Lavender', null, 'joan.lavender@gmail.com', null, 'hashed_password', 2);
 
 USE TICKETINGSYSTEMDB;
