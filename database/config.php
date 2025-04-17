bd59c714ff1f1edb0b2781a7bb0dad36df385d18
Lavender <gercermagden@gmail.com>
2025-04-16 22:33:01 -0500

    Fix casing of 'db_USER' to 'db_user' in database configuration

--- database/config.php
+++ database/config.php
@@ -3,6 +3,6 @@
 return [
     'db_host' => 'bd-g3.mysql.database.azure.com',
     'db_name' => 'TicketingSystemDB',
-    'db_USER' => 'g3Admin',
+    'db_user' => 'g3Admin',
     'db_pass' => 'Serfelizen29',
 ];
