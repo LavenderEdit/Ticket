da91046d5a5f5f771d105470235ba50ae2423eec
Lavender <gercermagden@gmail.com>
2025-04-25 13:24:03 -0500

    fix(scripts.php): Actualizar la versión del script principal para garantizar la compatibilidad con los cambios recientes.

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,5 +1,5 @@
 <script src="https://kit.fontawesome.com/2185a48098.js" crossorigin="anonymous"></script>
-<script type="module" src="js/main-script.js?v=6"></script>
+<script type="module" src="js/main-script.js?v=7"></script>
 <script src="js/lib/bootstrap/bootstrap.bundle.min.js" defer></script>
 <script src="js/lib/jquery/jquery-3.7.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
